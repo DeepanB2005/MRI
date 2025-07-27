c17b5660df8c1ddb5c7bfbbca436f46e3d6e260e
DeepanB2005 <deepanb2005@gmail.com>
2025-07-27 15:01:42 +0530

    chatbot

--- bc/server.js
+++ bc/server.js
@@ -8,10 +8,15 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get('api/first',(req,res)=>{
-  res.send("hello react this is express");
+app.get('/api/first',(req,res)=>{
+  res.json({mj:"hello react this is express"});
 });
 
+app.post('/api/ip',(req,res)=>{
+  const { ms } =req.body;
+  console.log("success");
+  res.json({st:"success",reply:`${ms}`})
+});
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
--- fr/src/App.jsx
+++ fr/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const send =()=> {
+  const [ms,setMs]=useState("");
+  
+  const send = async () => {
+    try {
+      const response = await fetch("/api/ip", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      const data = await response.json();
+      console.log("Server response:", data);
+    }catch(err)
+  {
+    console.log(err)
+  }}
+};
+
+  useEffect(()=>
+  {
+    fetch('/api/first')
+    .then(res =>res.json())
+    .then(data => setm(data.mj))
+    .catch(err => console.log("ip not get"))
+  },[]);
+
+  
 
   return (
+    // <>
+    //   <div className="bg-green-400 h-30 w-70 align-middle">
+    //   <h1>{m}</h1>
+    //   <button className="bg-red-300" onClick={()=>setCount("vanakkam da mapla")}>hower,click</button>
+    //   <h1 >{count}</h1>
+    //   <button className='bg-yellow-500 h-10 w-30'onClick={()=>setr(!cc)}>clickeffect</button>
+      
+    //   </div>
+    //   <div className={`h-50 w-50 mt-20 ${cc ? 'bg-red-300 animate-ping' : 'bg-green-400 animate-bounce'}`}></div>
+    // </>
     <>
-      <div className='bg-green-600'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-pink-200 rounded-3xl shadow-md shadow-gray-200 p-3 w-80 mx-auto' ><h1>WELCOME TO MY CHATAPP</h1></div>
+    <div className="sm:w-120 xl:w-130 h-[600px] p-5 bg-gradient-to-t from-red-200 to-green-200 mx-auto mt-10 rounded-3xl shadow-2xl" >
+      <div className='p-3 bg-gradient-to-r from-yellow-100 to-orange-200 w-full h-full rounded-2xl'> 
+        <div className='h-124'></div>
+        <div className='flex'>
+        <input type='text' value={ms} onChange={(l)=>setms(l.target.value)} className='flex flex-col h-10 w-80 bg-blue-100 rounded-l-2xl p-3 border-2 border-blue-300 hover:bg-amber-100 placeholder:enter your query' />
+        <button onClick={send} className='bg-blue-400 w-20 h-10 rounded-r-3xl flex flex-col border-green-400 text-center p-2 text-violet-500 font-extrabold hover:cursor-crosshair'>ASK</button>
       </div>
-      <h1 className='bg-green-800'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
