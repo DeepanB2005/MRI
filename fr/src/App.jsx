6dca8d04c7894b1466896c1129cb071118c94587
DeepanB2005 <deepanb2005@gmail.com>
2025-07-27 15:24:27 +0530

    chatbot fr

--- fr/src/App.jsx
+++ fr/src/App.jsx
@@ -4,8 +4,10 @@ import './App.css'
 import { useEffect, useState } from 'react';
 
 function App() {
-  const send =()=> {
-  const [ms,setMs]=useState("");
+    const [ms,setms]=useState("");
+    const [m,setm]=useState("");
+    const [reply,setreply]=useState("");
+
   
   const send = async () => {
     try {
@@ -14,16 +16,26 @@ function App() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ message }),
+        body: JSON.stringify({ ms }),
       });
 
       const data = await response.json();
       console.log("Server response:", data);
+      setreply(data.reply);
+      setms("");
+
     }catch(err)
   {
     console.log(err)
   }}
-};
+  const enter = (e) =>
+  {
+    if(e.key === "Enter")
+    {
+      send();
+    }
+  }
+
 
   useEffect(()=>
   {
@@ -50,10 +62,13 @@ function App() {
     <div className='bg-pink-200 rounded-3xl shadow-md shadow-gray-200 p-3 w-80 mx-auto' ><h1>WELCOME TO MY CHATAPP</h1></div>
     <div className="sm:w-120 xl:w-130 h-[600px] p-5 bg-gradient-to-t from-red-200 to-green-200 mx-auto mt-10 rounded-3xl shadow-2xl" >
       <div className='p-3 bg-gradient-to-r from-yellow-100 to-orange-200 w-full h-full rounded-2xl'> 
-        <div className='h-124'></div>
-        <div className='flex'>
-        <input type='text' value={ms} onChange={(l)=>setms(l.target.value)} className='flex flex-col h-10 w-80 bg-blue-100 rounded-l-2xl p-3 border-2 border-blue-300 hover:bg-amber-100 placeholder:enter your query' />
-        <button onClick={send} className='bg-blue-400 w-20 h-10 rounded-r-3xl flex flex-col border-green-400 text-center p-2 text-violet-500 font-extrabold hover:cursor-crosshair'>ASK</button>
+        <div className='h-124'>
+          {reply && 
+          <div className='bg-green-200 p-3 shadow-inner'><p>{reply}</p></div>}
+        </div>
+        <div className='flex rounded-3xl shadow-md'>
+        <input type='text' value={ms} onChange={(e) => setms(e.target.value)} onKeyDown={enter} className='flex flex-col h-10 w-80 bg-blue-100 rounded-l-2xl p-3 border-2 border-blue-300 hover:bg-amber-100 placeholder:enter your query' />
+        <button onClick={send} className='bg-blue-400 w-20 h-10 rounded-r-2xl flex flex-col border-green-400 text-center p-2 text-violet-500 font-extrabold hover:cursor-crosshair'>ASK</button>
       </div>
       </div>
     </div>
